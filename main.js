cb718ab7ff0628d7556cfb5f4ed334e16972950b
JakubCzechowski <kubaacze@gmail.com>
2019-03-27 19:39:08 +0100

    przed zmiana na divy

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 window.onload = function () {
 
 
-    list = document.getElementById('list')
+    var list = document.getElementById('list')
     loadData();
     function loadData() {
-        $.ajax({ url: 'http://api.openweathermap.org/data/2.5/weather?q=Poland,krakow&APPID=1998e2006e7bb2361c6bbfdcac64b33c' })
+        $.ajax({ url: 'http://api.openweathermap.org/data/2.5/forecast?q=Krakow,pl&APPID=1998e2006e7bb2361c6bbfdcac64b33c' })
             .done(resp => {
 
 
-                var el = document.createElement('li');
-                el.innerHTML = 'Temperature: ' + resp.main.temp;
-                list.appendChild(el);
+                //var el = document.createElement('li');
+                //el.innerHTML = 'Temperature: ' + Math.round(resp.list.main.temp  - 271) + '&#x2103';
+                //list.appendChild(el);
 
 
+                for (i = 0; i < resp.list.length; i++) {
+
+                    var el = document.createElement('li');
+                    el.innerHTML = resp.list[i].dt_txt +'\xa0\xa0\xa0'+ 'Temperature: ' + Math.round(resp.list[i].main.temp - 271) + '&#x2103';
+                    list.appendChild(el);
 
 
+                }
 
-
-
-
-            }).done(() => { loadData(); })
+            })
     }
 
 
 
 }
+
+
+dupsko
